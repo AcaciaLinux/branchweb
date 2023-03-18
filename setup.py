463de9b20853aae27b1336e2d769cf04c6006d7a
Max Kofler <kofler.max.dev@gmail.com>
2023-03-18 16:59:56 +0100

    Remove webauth

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     package_dir={'': 'src'},
     packages=['branchweb'],
-    py_modules=["webserver", "webauth", "usermanager"],
+    py_modules=["webserver", "usermanager"],
     include_package_data=True,
     install_requires=[],
     author='zimsneexh',
